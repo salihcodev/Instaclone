accf377c0af5985ba962a16f07ee9c928f93c742
salihcodev <ahmedsalihdev@gmail.com>
2020-09-30 11:59:35 +0200

    Setup all project files.

---
+++ src/firebase/firebase.confg.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+  apiKey: 'AIzaSyDjI9Il8RXRamyylXl4_9FBTVqrCq5iOk8',
+  authDomain: 'instaclone-101f8.firebaseapp.com',
+  databaseURL: 'https://instaclone-101f8.firebaseio.com',
+  projectId: 'instaclone-101f8',
+  storageBucket: 'instaclone-101f8.appspot.com',
+  messagingSenderId: '639265410353',
+  appId: '1:639265410353:web:395360818df315d1ebb410',
+  measurementId: 'G-XS2MSEWST4',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
